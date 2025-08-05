d3450eac4fd60706a099ab5497e4c9943ae6c505
zeon0xx0 <jihyon5615@naver.com>
2025-08-05 15:23:23 +0900

    1521

---
+++ src/main/java/pack/Spring02Application.java
@@ -0,0 +1,19 @@
+package pack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Spring02Application {
+	@GetMapping("/")
+	public String tiger() {
+		return "Spring02:1450";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(Spring02Application.class, args);
+	}
+
+}
---
+++ src/test/java/pack/Spring02ApplicationTests.java
@@ -0,0 +1,13 @@
+package pack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
