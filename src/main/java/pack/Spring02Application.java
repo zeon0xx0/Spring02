1b025e5f88aca89d8c2d092c0a6f03234919ddcb
zeon0xx0 <jihyon5615@naver.com>
2025-08-08 09:40:29 +0900

    0940

--- src/main/java/pack/Spring02Application.java
+++ src/main/java/pack/Spring02Application.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class Spring02Application {
 	@GetMapping("/")
 	public String tiger() {
-		return "Spring02:0913";
+		return "Spring02:0934";
 	}
 	public static void main(String[] args) {
 		SpringApplication.run(Spring02Application.class, args);
